37d92323748ed7701054b2515b82b68e42e1b28f
shubo7996 <whatsupmofo420@gmail.com>
2018-08-16 12:33:04 +0530

    keras model

--- cnn.py
+++ cnn.py
@@ -3,6 +3,7 @@ from keras.models import Convolution2D
 from keras.models import MaxPooling2D
 from keras.models import Flatten
 from keras.models import Dense
+from Keras.preprocessing import ImageDataGenerator
 
 classifier = Sequential()
 
@@ -13,3 +14,32 @@ classifier.add(Dense(output_dim=128, activation='relu'))
 classifier.add(Dense(output_dim=1,activation='sigmoid'))
 
 classifier.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+
+#Fitting the CNN to the images
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        'data/train',
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+        'data/validation',
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='binary')
+
+model.fit_generator(
+        train_generator,
+        steps_per_epoch=2000,
+        epochs=50,
+        validation_data=validation_generator,
+        validation_steps=800)
+
