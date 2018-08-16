8fad2c2c54f3a31b3acffb908305b585e5e9e113
shubo7996 <whatsupmofo420@gmail.com>
2018-08-16 12:41:06 +0530

    keras model

--- cnn.py
+++ cnn.py
@@ -24,22 +24,22 @@ train_datagen = ImageDataGenerator(
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
-train_generator = train_datagen.flow_from_directory(
-        'data/train',
-        target_size=(150, 150),
-        batch_size=32,
-        class_mode='binary')
+training_set = train_datagen.flow_from_directory(
+	'DataSet/training_set',
+    target_size=(64, 64),
+    batch_size=32,
+    class_mode='binary')
 
-validation_generator = test_datagen.flow_from_directory(
-        'data/validation',
-        target_size=(150, 150),
-        batch_size=32,
-        class_mode='binary')
+test_set = test_datagen.flow_from_directory(
+    'DataSet/test_set',
+    target_size=(64, 64),
+    batch_size=32,
+    class_mode='binary')
 
-model.fit_generator(
-        train_generator,
-        steps_per_epoch=2000,
-        epochs=50,
-        validation_data=validation_generator,
-        validation_steps=800)
+classifier.model.fit_generator(
+    training_set,
+    steps_per_epoch=8000,
+    epochs=25,
+    validation_data=test_set,
+    validation_steps=2000)
 
